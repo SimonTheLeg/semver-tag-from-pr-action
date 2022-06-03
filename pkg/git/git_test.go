7dd053cdf67f275ab17efa052859a6a4c802d1b3
Simon Bein <simontheleg@gmail.com>
2022-06-03 17:39:10 +0200

    fix: remove hardcoded integration test repo path

--- pkg/git/git_test.go
+++ pkg/git/git_test.go
@@ -1,29 +1,77 @@
 package git
 
 import (
+	"errors"
+	"os"
 	"testing"
 
 	"github.com/Masterminds/semver/v3"
 	gogit "github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
 )
 
-func TestFindLatestSemVerTag(t *testing.T) {
-	repoPath := "/Users/simonbein/github/SimonTheLeg/tag-on-merge-integration-infra"
-
-	repo, err := gogit.PlainOpen(repoPath)
-	if err != nil {
-		t.Fatal(err)
+func TestFindLatestSemVerTagIntegration(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Skipping integrationtest TestFindLatestSemVerTagIntegration")
 	}
+
+	repo := getIntegrationRepoForRef(t, "0a3f8c254543b1231bde79e5a2483a6b9a3d4081")
 
 	latestSemVer, err := FindLatestSemVerTag(repo)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	expSemver, _ := semver.NewVersion("v1.1.1")
+	expSemver, _ := semver.NewVersion("v1.1.0")
 
 	if !latestSemVer.Equal(expSemver) {
 		t.Errorf("Exp semVer %q, got %q", expSemver.String(), latestSemVer.String())
 	}
 
+}
+
+// returns the integration repo without the need to constantly clone it
+func getIntegrationRepoForRef(t *testing.T, ref string) *gogit.Repository {
+	const repostorespath = "/tmp/tag-on-merge-integration-infra"
+	const remotename = "origin"
+
+	// clone the repository, if it is not already present
+	var repo *gogit.Repository
+	var cloned bool
+	if _, err := os.Stat(repostorespath); os.IsNotExist(err) {
+		var err error
+		repo, err = gogit.PlainClone(repostorespath, false, &gogit.CloneOptions{
+			URL: "https://github.com/SimonTheLeg/tag-on-merge-integration-infra.git",
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
+		cloned = true
+	} else {
+		repo, err = gogit.PlainOpen(repostorespath)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	w, err := repo.Worktree()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// check out the desired commit
+	err = w.Checkout(&gogit.CheckoutOptions{Hash: plumbing.NewHash(ref)})
+	// if the commit does not exists and we have not cloned the repository, pull updates
+	if errors.Is(plumbing.ErrReferenceNotFound, err) && cloned == false {
+		err = w.Pull(&gogit.PullOptions{RemoteName: remotename})
+		if err != nil {
+			t.Fatal(err)
+		}
+		err = w.Checkout(&gogit.CheckoutOptions{Hash: plumbing.NewHash(ref)})
+	}
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return repo
 }
